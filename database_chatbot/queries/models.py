b49f6471f035d56def9de10a584868178c9c725b
chirag-singhal <csinghal208@gmail.com>
2020-06-06 15:24:43 +0530

    added query model

--- database_chatbot/queries/models.py
+++ database_chatbot/queries/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Query(models.Model):
+    intent = models.CharField(max_length=60)
+    entities = models.CharField(max_length=60)
+    response = models.CharField(max_length=200)
+
