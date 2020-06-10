fd4b9bb5eae001d02eed9b41346e82045103c6ef
Aman-Maheshwari7 <64311200+Aman-Maheshwari7@users.noreply.github.com>
2020-06-10 20:55:33 +0530

    Update response_generator_1.py

--- response_generator_1.py
+++ response_generator_1.py
@@ -9,7 +9,7 @@ def response_generation(s):
     from django.db import connection
     with connection.cursor() as cursor:
         sql_query=("SELECT response from Query WHERE intent=%s")
-        cursor.execute(sql_query,(s,))
+        cursor.execute(sql_query,[s])
         for x in cursor:
             s1=x
         s2=''.join(s1)
