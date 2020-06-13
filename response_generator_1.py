c5b48e33372d800130f00d400c2a0650097ffae3
Aman-Maheshwari7 <64311200+Aman-Maheshwari7@users.noreply.github.com>
2020-06-14 01:55:25 +0530

    Update response_generator_1.py

--- response_generator_1.py
+++ response_generator_1.py
@@ -2,7 +2,7 @@
 def response_generation(s):
     from django.db import connection
     with connection.cursor() as cursor:
-        sql_query=("SELECT val from test WHERE id=%s")
+        sql_query=("SELECT response from queries_query WHERE intent=%s")
         cursor.execute(sql_query,[s])
         for x in cursor:
             s1=x
